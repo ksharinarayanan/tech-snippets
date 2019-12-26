a500583999af60bede63e6c5d5d33ef07138dcce
techsnippets <techsnippets718@gmail.com>
2019-12-26 21:36:09 +0530

    random commit

--- pages/views.py
+++ pages/views.py
@@ -7,11 +7,14 @@ from blog.models import Blog
 def home_view(request, *args, **kwargs):
     featured = Blog.objects.all().order_by('id').reverse()[:1]
     
-    trending = Blog.objects.all().order_by('id').reverse()
+    trending = Blog.objects.all().order_by('id').reverse()[:4]
+
+    all_objects = Blog.objects.all().order_by('id').reverse()
 
     content = {
         'featured' : featured,
         'trending' : trending,
+        'all_blogs' : all_objects,
     }
 
     return render(request, 'home/landingpage.html', content)
