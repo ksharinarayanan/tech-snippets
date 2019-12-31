f7dcff8dda3fae1c937f6dcfcddd128f226568d5
techsnippets <techsnippets718@gmail.com>
2019-12-31 22:46:30 +0530

    Revert "debug false"
    This reverts commit 59b3056158819b724b41575a61f4541991975ac0.

--- TechSnippets/settings.py
+++ TechSnippets/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rt=_b%c25^_kmna*j331e7d3oayk&)t9%u@=6m3*3&#&5iw$t!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False   
+DEBUG = True   
 
 ALLOWED_HOSTS = ['*']
 
