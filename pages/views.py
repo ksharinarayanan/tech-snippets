00b93749d7fbd47c1b52607ad155f4da1bfcf153
ksharinarayanan <ksharinarayanan8@gmail.com>
2020-04-07 20:49:05 +0530

    redirect

--- pages/views.py
+++ pages/views.py
@@ -55,5 +55,4 @@ def about_view(request):
     return render(request, 'pages/about.html', {})
 
 def brave_view(request):
-    response = redirect("/static/.well-known/brave-rewards-verification.txt")
-    return response
+    return redirect("https://tech-snippets.azurewebsites.net/static/.well-known/brave-rewards-verification.txt")
